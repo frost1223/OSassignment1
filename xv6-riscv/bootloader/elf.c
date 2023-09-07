f7a5df998c91b0b007b6df4a217b3d19e668672f
Lakshman <lakshmanyadavpatakula@gmail.com>
2023-09-06 23:11:04 -0700

    2.4.2

--- xv6-riscv/bootloader/elf.c
+++ xv6-riscv/bootloader/elf.c
@@ -13,23 +13,23 @@ struct proghdr* kernel_phdr;
 
 uint64 find_kernel_load_addr(enum kernel ktype) {
     /* CSE 536: Get kernel load address from headers */
-    struct elfhdr* eh = (struct elfhdr *)RAMDISK;
-    struct proghdr* ph = (struct proghdr *)((uint64_t)eh + eh->phoff + eh->phentsize);
+    kernel_elfhdr = (struct elfhdr *)RAMDISK;
+    kernel_phdr = (struct proghdr *)((uint64_t)kernel_elfhdr + kernel_elfhdr->phoff + kernel_elfhdr->phentsize);
 
-    uint64_t kernload_start = ph->vaddr;
+    uint64_t kernload_start = kernel_phdr->vaddr;
 
     return kernload_start;
 }
 
 uint64 find_kernel_size(enum kernel ktype) {
     /* CSE 536: Get kernel binary size from headers */
-    struct elfhdr *eh = (struct elfhdr *)RAMDISK;
-    uint64_t kernel_size = (eh->shoff) + (eh->shnum)*(eh->shentsize) ;
+    kernel_elfhdr = (struct elfhdr *)RAMDISK;
+    uint64_t kernel_size = (kernel_elfhdr->shoff) + (kernel_elfhdr->shnum)*(kernel_elfhdr->shentsize) ;
     return kernel_size;
 }
 
 uint64 find_kernel_entry_addr(enum kernel ktype) {
     /* CSE 536: Get kernel entry point from headers */
     struct elfhdr *eh = (struct elfhdr *)RAMDISK;
-    return eh->entry;
+    return kernel_elfhdr->entry;
 }
